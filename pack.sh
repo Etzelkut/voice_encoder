41751e71ed1e54c7817fd3e9fd878191adca6cab
Etzelkut <aldeka12@gmail.com>
2021-04-21 05:51:45 +0600

    added pack

---
+++ pack.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pip install transformers
+pip install adabelief-pytorch==0.2.0
+pip install ranger-adabelief==0.1.0
+
+#!pip install torchtext#==0.8.1
+#==0.7.0
+pip install torchaudio
+#==1.2.2
+pip install pytorch-lightning
+#==3.4.0
+
+conda remove wrapt
+
+pip install comet-ml
+
