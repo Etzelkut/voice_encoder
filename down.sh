38b0768067e03c50fd24f7893108dcfe63dde802
Etzelkut <aldeka12@gmail.com>
2021-04-21 05:00:50 +0600

    small change

--- down.sh
+++ down.sh
@@ -6,8 +6,6 @@ mkdir weights
 pip install gdown
 gdown https://drive.google.com/uc?id=1qZ9NEJfc8AQ20ht88QX2ZlxXOoT79aMY
 
-unzip -qq vox1_dev_wav.zip.zip -d data
-
 wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_small_960h.pt
 pip install transformers
 pip install adabelief-pytorch==0.2.0
@@ -21,3 +19,4 @@ pip install pytorch-lightning
 #==3.4.0
 pip install comet-ml
 
+unzip -qq vox1_dev_wav.zip -d data
