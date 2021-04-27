679e1b1d433ba91a3b11c540b83c1c5fed7ca319
Etzelkut <aldeka12@gmail.com>
2021-04-27 06:11:17 +0600

    minor

--- downtest.sh
+++ downtest.sh
@@ -5,6 +5,9 @@ mkdir test
 pip install gdown
 
 gdown https://drive.google.com/uc?id=1eKKoKgYsfg2PqqSb-No5h1fkX-2SyHHa
-gdown https://drive.google.com/uc?id=1uZAG1-LO_8nFutPmw4IEDHTmuv_vHJPK -O /content/test/test_division.txt
+gdown https://drive.google.com/uc?id=1uZAG1-LO_8nFutPmw4IEDHTmuv_vHJPK
+
+
+mv veri_test.txt /test/test_division.txt
 
 unzip -qq vox1_test_wav.zip -d test
