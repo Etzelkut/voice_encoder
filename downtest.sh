97b4bade0fb372f8f7bacf7fe9f73f1b4a71987d
Etzelkut <aldeka12@gmail.com>
2021-04-27 06:14:42 +0600

    minor

--- downtest.sh
+++ downtest.sh
@@ -8,6 +8,6 @@ gdown https://drive.google.com/uc?id=1eKKoKgYsfg2PqqSb-No5h1fkX-2SyHHa
 gdown https://drive.google.com/uc?id=1uZAG1-LO_8nFutPmw4IEDHTmuv_vHJPK
 
 
-mv veri_test.txt /test/test_division.txt
+mv veri_test.txt /home/data/voice_encoder/voice_encoder/test/test_division.txt
 
 unzip -qq vox1_test_wav.zip -d test
